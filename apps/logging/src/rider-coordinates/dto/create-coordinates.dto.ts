e51ac4c06f79ac2cc77625a68dbe2198bfa6814a
Austine The-Enigma <austine574@gmail.com>
2026-03-22 18:36:39 +0100

    added more valdators to dto

--- apps/logging/src/rider-coordinates/dto/create-coordinates.dto.ts
+++ apps/logging/src/rider-coordinates/dto/create-coordinates.dto.ts
@@ -4,10 +4,12 @@ import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 export class CreateCoordinatesDto {
   @Type(() => Number)
   @IsNumber()
+  @IsNotEmpty()
   lat: number;
 
   @Type(() => Number)
   @IsNumber()
+  @IsNotEmpty()
   long: number;
 
   @IsString()
